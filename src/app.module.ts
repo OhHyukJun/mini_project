49f0c8db3c61cb7e131037d079090d21b71b9242
OhHyukJun <kmh7277@naver.com>
2024-05-29 14:59:53 +0900

    Feat: mysql 연동 코드

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.RDS_HOST,
+      port: 3306,
+      username: process.env.RDS_USER,
+      password: process.env.RDS_PSWORD,
+      database: process.env.RDS_DATABASE,
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
